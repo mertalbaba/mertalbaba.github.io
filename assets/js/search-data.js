17ff4c74523c4a8c1cb25d452d8f9dbb056a37f0
mertalbaba <mertalbaba@gmail.com>
2025-01-14 12:12:55 +0000

    Deploying to gh-pages from @ mertalbaba/mertalbaba.github.io@88a8b79cee84b521360e097a7cde28c5bbf07dce 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -64,7 +64,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%65%72%74%61%6C%62%61%62%61@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -72,6 +72,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/mertalbaba", "_blank");
         },
       },{
         id: 'social-rss',
@@ -85,7 +92,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=AeAVp7YAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/brtmertalb", "_blank");
         },
       },{
         id: 'social-custom_social',
