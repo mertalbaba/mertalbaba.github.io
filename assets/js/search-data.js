5d4d2df82ccfd8a7ef3b74d134a1ed666b55ef48
mertalbaba <mertalbaba@gmail.com>
2025-04-24 17:55:30 +0000

    Deploying to gh-pages from @ mertalbaba/mertalbaba.github.io@0c1e572a39c5918468501b355d7ea4b99e095d2b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,8 +52,8 @@ ninja.data = [{
           window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
         
       },
-    },{id: "projects-nil-no-data-imitation-leatning",
-          title: 'NIL - No-data Imitation Leatning',
+    },{id: "projects-nil-no-data-imitation-learning",
+          title: 'NIL - No-data Imitation Learning',
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_nil/";
