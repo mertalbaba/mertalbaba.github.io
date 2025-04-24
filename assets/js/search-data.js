b54945d347ecdfb870c5d65ae77a8c0302a73c66
mertalbaba <mertalbaba@gmail.com>
2025-04-24 16:40:33 +0000

    Deploying to gh-pages from @ mertalbaba/mertalbaba.github.io@e4545367f643df489e7bcbbe211ec6edf9af75eb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "My PhD Research Projects",
+          description: "selected research projects",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -52,8 +52,8 @@ ninja.data = [{
           window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
         
       },
-    },{id: "projects-",
-          title: '',
+    },{id: "projects-rile-amp-colon-reinforced-imitation-learning",
+          title: 'RILe &amp;amp;colon Reinforced Imitation Learning',
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_rile/";
