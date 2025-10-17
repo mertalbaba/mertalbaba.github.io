a59d817008c051ca628eb9cbe47aaf69d52ee311
Mert Albaba <mertalbaba@gmail.com>
2025-10-17 19:50:14 +0200

    update latex

--- assets/js/mathjax-setup.js
+++ assets/js/mathjax-setup.js
@@ -1,5 +1,20 @@
 window.MathJax = {
   tex: {
     tags: "ams",
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']],
+    processEscapes: true,
+    processEnvironments: true,
   },
+  options: {
+    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre'],
+  },
+  startup: {
+    ready: () => {
+      MathJax.startup.defaultReady();
+      MathJax.startup.promise.then(() => {
+        console.log('MathJax initial typesetting complete');
+      });
+    }
+  }
 };
